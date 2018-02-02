fe5d3a903b1abf627111c380ee6a6e0356d73862
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-02-02 15:51:52 +0530

    to check whether a character is an alphabet or not

---
+++ alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char k;
+    printf("Enter a character:\n ");
+    scanf("%c",&k);
+    if( (k>='a' && k<='z') || (k>='A' && k<='Z'))
+        printf("%c is an alphabet.",k);
+    else
+        printf("%c is not an alphabet.",k);
+    return 0;
+}
