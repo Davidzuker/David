aabe1dd0f1db2b515f99a7ad177d765fb1ab7338
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-02-07 14:55:12 +0530

    to display armstrong number between two intervals

--- arm.c
+++ arm.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
-    int number, originalNumber, remainder, result = 0;
-    printf("Enter a three digit integer: ");
-    scanf("%d", &number);
-    originalNumber = number;
-    while (originalNumber != 0)
+    int a, b, i, temp1, temp2, r, n = 0, result = 0;
+
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &a, &b);
+    printf("Armstrong numbers between %d an %d are: ", a, b);
+    for(i = low + 1; i < high; ++i)
     {
-        remainder = originalNumber%10;
-        result += remainder*remainder*remainder;
-        originalNumber /= 10;
+        temp2 = i;
+        temp1 = i;
+        // number of digits calculation
+        while (temp1 != 0)
+        {
+            temp1 /= 10;
+            ++n;
+        }
+        // result contains sum of nth power of its digits
+        while (temp2 != 0)
+        {
+            remainder = temp2 % 10;
+            result += pow(remainder, n);
+            temp2 /= 10;
+        }
+        // checks if number i is equal to the sum of nth power of its digits
+        if (result == i) {
+            printf("%d ", i);
+        }
+        // resetting the values to check Armstrong number for next iteration
+        n = 0;
+        result = 0;
     }
-    if(result == number)
-        printf("%d is an Armstrong number.",number);
-    else
-        printf("%d is not an Armstrong number.",number);
     return 0;
 }
