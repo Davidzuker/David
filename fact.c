b747199a3f9a7f27cb0ce3947a936a8106a6ae34
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-02-07 14:57:49 +0530

    to display a factorial of a anumber

---
+++ fact.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a, i;
+    unsigned long long factorial = 1;
+    printf("Enter an integer: ");
+    scanf("%d",&a);
+    // show error if the user enters a negative integer
+    if (a < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else
+    {
+        for(i=1; i<=a; ++i)
+        {
+            factorial *= i;              // factorial = factorial*i;
+        }
+        printf("Factorial of %d = %llu", n, factorial);
+    }
+    return 0;
+}
