bbc8c40e6ced6e3b22d5b1ee4a58ca79d53170ab
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-02-09 16:08:25 +0530

    to print a number

--- subtract.c
+++ subtract.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void) {
-	int a,b,c,d,min1,min2,tmin1,tmin2,sub,result,result1;
-	scanf("%d %d",&a,&b);
-	scanf("%d %d",&c,&d);
-	min1=a*60;
-	min2=c*60;
-	tmin1=min1+b;
-	tmin2=min2+d;
+	int m,n,o,p,min1,min2,tmin1,tmin2,sub,result,result1;
+	scanf("%d %d",&m,&n);
+	scanf("%d %d",&o,&p);
+	min1=m*60;
+	min2=o*60;
+	tmin1=min1+n;
+	tmin2=min2+p;
 	sub=tmin1-tmin2;
 	result=sub/60;
 	printf("%d\t",result);
