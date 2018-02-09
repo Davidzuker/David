939b68aae4e8f4cae7e34012cb75240148c6c947
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-02-09 16:19:41 +0530

    to count the number of lines in paragraph

---
+++ paragraph.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+	char o[50];
+	int i,count=1;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",o);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(o[i]=='.')
+		{
+			count=count+1;
+		}
+	}
+	printf("\no of lines is=%d",count);
+	return 0;
+}
