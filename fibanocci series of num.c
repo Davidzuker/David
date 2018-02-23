17de8cf6be99387f22b5e9cea6a501aa666db702
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-02-23 16:03:52 +0530

    Update fibanocci series of num.c

--- fibanocci series of num.c
+++ fibanocci series of num.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(void) 
 {
-int a=1,b=1,i,n,c;
+int s=1,d=1,i,n,c;
 printf("\n enter the range to print the Fibonacci numbers\n");
 scanf("%d",&n);
-printf("%d",a);
-printf("\t%d",b);
+printf("%d",s);
+printf("\t%d",d);
 for(i=1;i<=n-2;i++)
 {
-c=a+b;
+c=s+d;
 printf("\t%d",c);
-a=b;
-b=c;
+s=d;
+d=c;
 }
 return 0;
 }
