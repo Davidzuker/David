a645e9679e94f5d28e53777b8dc8619142eb8ae9
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-03-09 15:59:19 +0530

    Update alphapneumatic.c

--- alphapneumatic.c
+++ alphapneumatic.c
@@ -3,11 +3,11 @@
 int main() 
 {
 	char str[50];
-	int i,n;
+	int i,k;
 	printf("\nenter the alphanumeric string:\n");
 	scanf("%s",str);
 	n=strlen(str);
-	for(i=0;i<n;i++)
+	for(i=0;i<k;i++)
 	{
 		if(str[i]>='0' && str[i]<='9')
 		{
