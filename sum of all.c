171d88705fe2e224eb73c8b8872d688791e79c91
Davidzuker <35991655+Davidzuker@users.noreply.github.com>
2018-03-14 15:44:32 +0530

    Update sum of all.c

--- sum of all.c
+++ sum of all.c
@@ -2,15 +2,27 @@
 int main() 
 {
 	int n,array[50],i,sum=0;
+	
 	scanf("%d",&n);
+	
 	for(i=0;i<n;i++)
+	
 	{
+	
 		scanf("%d",&array[i]);
+	
 	}
+	
 	for(i=0;i<n;i++)
+
 	{
+	
 		sum=sum+array[i];	
+	
 	}
+	
 	printf("\n%d",sum);
+	
 	return 0;
+
 }
